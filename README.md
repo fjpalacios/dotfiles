5ed3e4635c9ed16ac43e399f0fd83e846bea702e
Javi Palacios <javi@fjp.es>
2018-06-09 19:43:20 +0200

    Replacing the bash script which do the magic by a Python one

--- README.md
+++ README.md
@@ -3,5 +3,4 @@
 This is a backup of all my configuration files.
 
 Thanks to…
-* [the smalley creative blog](http://blog.smalleycreative.com/tutorials/using-git-and-github-to-manage-your-dotfiles/) for the bash script to backup existing dotfiles in home directory.
 * [@mathiasbynens](https://github.com/mathiasbynens) for shell prompt and Git repository details from his [dotfiles](https://github.com/mathiasbynens/dotfiles).
---
+++ makelinks.py
@@ -0,0 +1,22 @@
+from subprocess import call
+from os import path
+
+
+DIRECTORY = ".dotfiles"
+OLD_DIRECTORY = ".dotfiles_old"
+FILES = ["bash_profile", "vimrc", "gitconfig", "gitexcludes", "pylintrc",
+         "zshrc", "tmux.conf", "imwheelrc"]
+
+print("Creating ~/{} for backup of any existing dotfiles at home"
+      .format(OLD_DIRECTORY))
+call(["mkdir", "-p", path.join(path.expanduser("~"), OLD_DIRECTORY)])
+call(["cd", path.join(path.expanduser("~"), DIRECTORY)])
+
+print("Moving any existing dotfiles from home to ~/{}".format(OLD_DIRECTORY))
+
+for file in FILES:
+    call(["mv", path.join(path.expanduser("~") + "/." + file),
+          path.join(path.expanduser("~"), OLD_DIRECTORY)])
+    print("Creating a symlink to {} in home directory".format(file))
+    call(["ln", "-s", path.join(path.expanduser("~"), DIRECTORY, file),
+          path.join(path.expanduser("~"), "." + file)])
