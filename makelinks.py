c45581d55b6fa84e25595ad890ae8f7c3f9366a7
Javi Palacios <javi@fjp.es>
2019-12-20 13:04:26 +0100

    Code refactor

--- makelinks.py
+++ makelinks.py
@@ -1,32 +1,38 @@
+from os.path import expanduser
+from os.path import isdir
+from os.path import join
 from subprocess import call
-from os import path
-from pathlib import Path
 
+DIRECTORY = '~/.dotfiles'
+OLD_DIRECTORY = '~/.dotfiles_old'
+FILES = {
+    '.vimrc': '~/',
+    '.gitconfig': '~/',
+    '.gitexcludes': '~/',
+    '.pylintrc': '~/',
+    '.zshrc': '~/',
+    '.tmux.conf': '~/',
+    '.imwheelrc': '~/',
+    '.chunkwmrc': '~/',
+    '.skhdrc': '~/',
+    'init.vim': '~/.config/nvim/'
+}
 
-DIRECTORY = '.dotfiles'
-OLD_DIRECTORY = '.dotfiles_old'
-FILES = ['vimrc', 'gitconfig', 'gitexcludes', 'pylintrc', 'zshrc', 'tmux.conf',
-         'imwheelrc', 'chunkwmrc', 'skhdrc', 'init.vim']
+print(f'Creating {OLD_DIRECTORY} to backup any existing dotfiles at home')
 
-print(f'Creating ~/{OLD_DIRECTORY} to backup any existing dotfiles at home')
+call(['mkdir', '-p', join(expanduser(OLD_DIRECTORY))])
 
-call(['mkdir', '-p', path.join(Path.home(), OLD_DIRECTORY)])
-call(['cd', path.join(Path.home(), DIRECTORY)], shell=True)
+for path in FILES.values():
+    if not isdir(expanduser(path)):
+        call(['mkdir', '-p', expanduser(path)])
 
-call(['mkdir', '-p', path.join(Path.home(), '.config', 'nvim')])
+print(f'Moving any existing dotfiles from home to {OLD_DIRECTORY}')
 
-print('Moving any existing dotfiles from home to ~/{}'.format(OLD_DIRECTORY))
+for file, path in FILES.items():
+    file_path = join(expanduser(path), file)
+    dotfile = join(expanduser(DIRECTORY), file)
+    old_directory = join(expanduser(OLD_DIRECTORY))
 
-for file in FILES:
-    if file == 'init.vim':
-        call(['mv', path.join(str(Path.home()), '.config', 'nvim', file),
-              path.join(Path.home(), OLD_DIRECTORY)])
-        print(f'Creating a symlink to {file} within its .config directory')
-        call(['ln', '-s', path.join(Path.home(), DIRECTORY, file),
-             path.join(Path.home(), '.config', 'nvim', file)])
-    else:
-        call(['mv', path.join(str(Path.home()), '.' + file),
-             path.join(Path.home(), OLD_DIRECTORY)])
-        print(f'Creating a symlink to .{file} in home directory')
-        call(['ln', '-s', path.join(Path.home(), DIRECTORY, file),
-             path.join(Path.home(), '.' + file)])
+    call(['mv', file_path, old_directory])
+    print(f'Creating a symlink to {file} in {path}')
+    call(['ln', '-s', dotfile, file_path])
